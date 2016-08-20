366ea9380589aca026072c37dc662e0dd41c9b25
Michael da Costa Silva <mdcs@hotmail.com>
2016-08-20 11:42:36 -0300

    Location

--- CortaPraMim/OndeViewController.swift
+++ CortaPraMim/OndeViewController.swift
@@ -9,14 +9,66 @@
 import UIKit
 import GoogleMaps
 
-class OndeViewController: UIViewController {
+class OndeViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate  {
     
     @IBOutlet weak var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
+    var listaEstabelecimentos = [Estabelecimento]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
         mapView.myLocationEnabled = true
         mapView.settings.myLocationButton = true
+        
+        locationManager.startUpdatingLocation()
+        
+        carregarEstabelecimentos()
+        
     }
     
+    func carregarEstabelecimentos() {
+        for estab in listaEstabelecimentos {
+            
+        }
+    }
+
+    
+    //MARK: - Maps & Location methods
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        locationManager.stopUpdatingLocation()
+        
+        if let location = locations.first! as CLLocation! {
+            dispatch_async(dispatch_get_main_queue(), {
+                self.mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            })
+        }
+    }
+    
+    func mapView(mapView: GMSMapView, didTapAtCoordinate coordinate: CLLocationCoordinate2D) {
+        
+    }
+    
+    func mapView(mapView: GMSMapView, didTapMarker marker: GMSMarker) -> Bool {
+        
+        return false
+    }
+    
+    func mapView(mapView: GMSMapView, didTapInfoWindowOfMarker marker: GMSMarker) {
+
+    }
 }
