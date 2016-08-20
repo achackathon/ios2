4011497a8d8aaf8af429ed5c90a9462f8b4a7f2b
Fransber Assuncao <fransber_assuncao@hotmail.com>
2016-08-20 10:01:46 -0300

    adicionado itens para listagem

---
+++ CortaPraMim/Estabelecimentos.swift
@@ -0,0 +1,18 @@
+//
+//  Estabelecimentos.swift
+//  CortaPraMim
+//
+//  Created by Fransber Assuncao on 20/08/16.
+//  Copyright © 2016 Team12. All rights reserved.
+//
+
+import UIKit
+
+class Estabelecimento: NSObject {
+    
+    var Nome: String?
+    var Servico: String?
+    var Valor: Double?
+
+    
+}
